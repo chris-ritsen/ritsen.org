73f56ab8af3be389b5a00453487aa0166efb22b9
Christopher Ritsen <chris.ritsen@gmail.com>
2016-08-24 17:31:49 -0400

    Use webpack to bundle index.js

--- server.js
+++ server.js
@@ -4,7 +4,12 @@ let Express = require("express");
 let app = new Express();
 let port = process.env.PORT || 3000;
 
-app.get("*", (request, response) => response.sendFile(__dirname + "/index.html"))
+app.use("/static", Express.static("static"));
+app.get("*", (request, response) => response.sendFile(__dirname + "/index.html"));
 
-app.listen(port);
+app.listen(port, (error) => {
+  if (error) {
+    console.log(error);
+  }
+});
 
