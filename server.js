1d2dc478070a0b1418ecbcd3f6744c2ed977f55e
Christopher Ritsen <chris.ritsen@gmail.com>
2016-08-24 23:17:25 -0400

    Fixed server routes

--- server.js
+++ server.js
@@ -4,8 +4,8 @@ let Express = require("express");
 let app = new Express();
 let port = process.env.PORT || 3000;
 
-app.use(Express.static("css-challenge"));
 app.get("/css-challenge/", (request, response) => response.sendFile(__dirname + "/css-challenge/index.html"));
+app.get("/css-challenge/stylesheet.css", (request, response) => response.sendFile(__dirname + "/css-challenge/stylesheet.css"));
 app.use("/static", Express.static("static"));
 app.get("/main.css", (request, response) => response.sendFile(__dirname + "/main.css"));
 app.get("/*", (request, response) => response.sendFile(__dirname + "/index.html"));
