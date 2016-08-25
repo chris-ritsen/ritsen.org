b4dd1b815a2abbde919f3dbe7dcfdac1610a58df
Christopher Ritsen <chris.ritsen@gmail.com>
2016-08-24 22:29:15 -0400

    CSS challenge files added

--- server.js
+++ server.js
@@ -4,6 +4,8 @@ let Express = require("express");
 let app = new Express();
 let port = process.env.PORT || 3000;
 
+app.use(Express.static("css-challenge"));
+app.get("/css-challenge/", (request, response) => response.sendFile(__dirname + "/css-challenge/index.html"));
 app.use("/static", Express.static("static"));
 app.get("/main.css", (request, response) => response.sendFile(__dirname + "/main.css"));
 app.get("/*", (request, response) => response.sendFile(__dirname + "/index.html"));
